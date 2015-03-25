192aa8f8710825da9ef97806482b3bf529c7f109
Jason Wilder <jason@influxdb.com>
2015-03-25 15:07:39 -0600

    Rename graphite/collect bind address config var name
    Make it consistent w/ pre-existing "bind-address config options.

--- cmd/influxd/config.go
+++ cmd/influxd/config.go
@@ -392,8 +392,8 @@ func ParseConfig(s string) (*Config, error) {
 }
 
 type Collectd struct {
-	Addr string `toml:"address"`
-	Port uint16 `toml:"port"`
+	BindAddress string `toml:"bind-address"`
+	Port        uint16 `toml:"port"`
 
 	Database string `toml:"database"`
 	Enabled  bool   `toml:"enabled"`
@@ -402,7 +402,7 @@ type Collectd struct {
 
 // ConnnectionString returns the connection string for this collectd config in the form host:port.
 func (c *Collectd) ConnectionString(defaultBindAddr string) string {
-	addr := c.Addr
+	addr := c.BindAddress
 	// If no address specified, use default.
 	if addr == "" {
 		addr = defaultBindAddr
@@ -418,8 +418,8 @@ func (c *Collectd) ConnectionString(defaultBindAddr string) string {
 }
 
 type Graphite struct {
-	Addr string `toml:"address"`
-	Port uint16 `toml:"port"`
+	BindAddress string `toml:"bind-address"`
+	Port        uint16 `toml:"port"`
 
 	Database      string `toml:"database"`
 	Enabled       bool   `toml:"enabled"`
@@ -431,7 +431,7 @@ type Graphite struct {
 // ConnnectionString returns the connection string for this Graphite config in the form host:port.
 func (g *Graphite) ConnectionString(defaultBindAddr string) string {
 
-	addr := g.Addr
+	addr := g.BindAddress
 	// If no address specified, use default.
 	if addr == "" {
 		addr = defaultBindAddr
